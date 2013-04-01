ba0c5079aace55cf720c36671593febda3d87585
Matthias Klumpp <matthias@tenstral.net>
2013-04-02 00:24:47 +0200

    trivial: Fix Python shebang

--- scripts/tanglu/dak-trac-bridge.py
+++ scripts/tanglu/dak-trac-bridge.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Copyright (C) 2013 Matthias Klumpp <mak@debian.org>
 
 # Licensed under the GNU Lesser General Public License Version 3
@@ -14,7 +15,6 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
-#!/usr/bin/python
 
 import os
 import sys
