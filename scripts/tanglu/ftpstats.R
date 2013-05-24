3fad4a53ffa1ffbf466745a373ac8b48925b5e8b
Matthias Klumpp <matthias@tenstral.net>
2013-05-24 03:35:36 +0200

    tanglu: Adjust statistics R script

--- scripts/tanglu/ftpstats.R
+++ scripts/tanglu/ftpstats.R
@@ -1,5 +1,4 @@
-arch <- c("source", "all", "amd64", "i386", "alpha", "arm", "armel", "armhf", "hppa", "hurd-i386", "ia64",
-	"kfreebsd-amd64", "kfreebsd-i386", "mips", "mipsel", "powerpc", "s390", "s390x", "sparc")
+arch <- c("source", "all", "amd64", "i386")
 palette(c("midnightblue", "gold", "turquoise", "cyan", "black", "red", "OrangeRed", "green3", "blue", "magenta",
 	"cornsilk3", "darkolivegreen3", "tomato4", "violetred2","thistle4", "steelblue2", "springgreen4",
 	"salmon","gray"))
@@ -11,7 +10,7 @@ plotsize <- function(file,title,p,height=11.8,width=16.9) {
 	legend(par("usr")[1]+xinch(5),par("usr")[4]-yinch(0.1),legend=colnames(t),
 		ncol=2,fill=1:19,xjust=1,yjust=1)
 }
-t <- (read.table("/srv/ftp-master.tanglu.org/misc/ftpstats.data",sep=",",header=0,row.names=1,col.names=cname))/1024/1024
+t <- (read.table("/srv/dak/misc/ftpstats.data",sep=",",header=0,row.names=1,col.names=cname))/1024/1024
 v <- t[(length(t$all)-90):(length(t$all)),1:19]
 
 plotsize("/srv/ftp-master.tanglu.org/web/size-quarter.png","Daily dinstall run size by arch (past quarter)",v)
